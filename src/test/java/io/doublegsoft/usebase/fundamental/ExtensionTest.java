7c491288b18afef87cf22ef1e807a671e359b8b5
ganguo <ganguo>
2026-05-20 19:28:45 +0800

    daily development

--- src/test/java/io/doublegsoft/usebase/fundamental/ExtensionTest.java
+++ src/test/java/io/doublegsoft/usebase/fundamental/ExtensionTest.java
@@ -1,5 +1,46 @@
 package io.doublegsoft.usebase.fundamental;
 
-public class ExtensionTest {
+import com.doublegsoft.jcommons.metabean.ModelDefinition;
+import com.doublegsoft.jcommons.metabean.ObjectDefinition;
+import com.doublegsoft.jcommons.metamodel.*;
+import io.doublegsoft.usebase.TestBase;
+import io.doublegsoft.usebase.Usebase;
+import org.junit.Assert;
+import org.junit.Test;
+
+public class ExtensionTest extends TestBase {
+
+  @Test
+  public void test_only_master() throws Exception {
+    ModelDefinition dataModel = loadModel("fundamental/extension");
+    String expr = loadUsebaseExpression("fundamental/extension#only_master");
+    UsecaseDefinition usecase = new Usebase(dataModel).parse(expr).get(0);
+    usecase.setModule("extension");
+    System.out.println(usecase.getOriginalText());
+
+    Assert.assertEquals(6, usecase.getStatements().size());
+    Assert.assertFalse(usecase.getStatements().get(0).isExceptional());
+    Assert.assertTrue(usecase.getStatements().get(1).isExceptional());
+
+    AssignmentDefinition stmtEncrypt = (AssignmentDefinition) usecase.getStatements().get(0);
+    AssignmentDefinition stmtFind = (AssignmentDefinition) usecase.getStatements().get(1);
+    AssignmentDefinition stmtCopy = (AssignmentDefinition) usecase.getStatements().get(2);
+
+    Assert.assertEquals("password", stmtEncrypt.getAssignee().getName());
+    Assert.assertEquals("string", stmtEncrypt.getAssignee().getType().getName());
+
+    Assert.assertEquals("user", stmtFind.getAssignee().getName());
+    Assert.assertEquals("user", stmtFind.getAssignee().getType().getName());
+
+    Assert.assertEquals("ret", stmtCopy.getAssignee().getName());
+    Assert.assertEquals("online_user", stmtCopy.getAssignee().getType().getName());
+
+    ObjectDefinition objVal = stmtCopy.getValue().getObjectValue();
+    Assert.assertNotNull(objVal);
+    Assert.assertEquals("user", objVal.getLabelledOption("original", "source"));
+    // 变量验证
+    VariableDefinition var = usecase.getVariable("ret");
+    Assert.assertEquals("online_user", var.getType().getName());
+  }
 
 }
